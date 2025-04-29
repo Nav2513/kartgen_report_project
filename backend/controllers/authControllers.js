c45f26594940e4d056151a90b7b4142bac318b2f
Nav2513 <134849961+Nav2513@users.noreply.github.com>
2025-04-30 03:26:22 +0530

    Adding login logic

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -24,3 +24,31 @@ exports.signup = async (req, res) => {
         res.status(500).json({ message: "Server error"});
     }
 }
+
+exports.login = async (req, res) => {
+    const {email, password} = req.body;
+
+    if(!email || !password)
+        return res.status(400).json({ message: "Email and password required"});
+
+    try {
+        const { rows } = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+
+        if(rows.length === 0)
+            return res.status(404).json({ message: "User not found!"});
+
+        const isMatch = await bcrypt.compare(password, rows[0].password);
+
+        if(!isMatch)
+            return res.status(401).json({ message: "Invalid credentials!"});
+
+        const token = jwt.sign({ userId: rows[0].id}, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRES_IN,
+        });
+
+        res.status(200).json({ token });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server error"});
+    };
+}
